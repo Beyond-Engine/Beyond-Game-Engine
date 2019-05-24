51ad2a84eb526fa4c0f567b3c34ea1454cf19ba0
Yu Li <liyutommy@outlook.com>
2019-05-24 15:56:55 -0600

    insertion to SparseMap

--- Engine/core/test/sparse_map_test.cpp
+++ Engine/core/test/sparse_map_test.cpp
@@ -1,4 +1,4 @@
-#include "core/ecs/sparse_set.hpp"
+﻿#include "core/ecs/sparse_set.hpp"
 
 namespace beyond {
 
@@ -7,24 +7,50 @@ public:
   using Trait = EntityTrait<Entity>;
   using SizeType = typename Trait::Id;
   using DiffType = typename Trait::DiffType;
+  using ValueType = T;
 
   [[nodiscard]] auto empty() const noexcept -> bool
   {
-    return entities_.empty();
+    return entities_set_.empty();
   }
 
   [[nodiscard]] auto size() const noexcept -> SizeType
   {
-    return entities_.size();
+    return entities_set_.size();
   }
 
   [[nodiscard]] auto capacity() const noexcept -> SizeType
   {
-    return entities_.capacity();
+    return entities_set_.capacity();
+  }
+
+  auto reserve(SizeType size) -> void
+  {
+    return entities_set_.reserve(size);
+  }
+
+  auto insert(Entity entity, ValueType /*data*/) -> void
+  {
+    return entities_set_.insert(entity);
+  }
+
+  [[nodiscard]] auto contains(Entity entity) const noexcept -> bool
+  {
+    return entities_set_.contains(entity);
+  }
+
+  [[nodiscard]] auto get(Entity entity) const noexcept -> SizeType
+  {
+    return entities_set_.get(entity);
+  }
+
+  [[nodiscard]] auto entities() const noexcept -> const Entity*
+  {
+    return entities_set_.entities();
   }
 
 private:
-  SparseSet<Entity> entities_;
+  SparseSet<Entity> entities_set_;
 };
 
 } // namespace beyond
@@ -40,4 +66,31 @@ TEST_CASE("SparseMap", "[beyond.core.ecs.sparse_set]")
   REQUIRE(sm.empty());
   REQUIRE(sm.size() == 0);
   REQUIRE(sm.capacity() == 0);
+
+  WHEN("Reserve space of 16 elements")
+  {
+    sm.reserve(16);
+    REQUIRE(sm.empty());
+    REQUIRE(sm.capacity() >= 16);
+  }
+
+  GIVEN("An entity and its corresponding data")
+  {
+
+    const Entity entity = 42;
+    const float data = 3.14f;
+
+    WHEN("Insert that entity to the sparse map")
+    {
+      sm.insert(entity, data);
+
+      THEN("You can find this entity in the sparse set")
+      {
+        REQUIRE(sm.contains(entity));
+        const auto index = sm.get(entity);
+        REQUIRE(index == 0);
+        REQUIRE(sm.entities()[index] == entity);
+      }
+    }
+  }
 }
