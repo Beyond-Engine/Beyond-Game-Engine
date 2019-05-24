e63a74ef40b095bf4ccd6569513e2c21302613fe
Yu Li <liyutommy@outlook.com>
2019-05-24 16:18:28 -0600

    erase from SparseMap

--- Engine/core/test/sparse_map_test.cpp
+++ Engine/core/test/sparse_map_test.cpp
@@ -26,12 +26,21 @@ public:
 
   auto reserve(SizeType size) -> void
   {
-    return entities_set_.reserve(size);
+    entities_set_.reserve(size);
   }
 
-  auto insert(Entity entity, ValueType /*data*/) -> void
+  auto insert(Entity entity, ValueType data) -> void
   {
-    return entities_set_.insert(entity);
+    entities_set_.insert(entity);
+    data_.push_back(std::move(data));
+  }
+
+  auto erase(Entity entity) -> void
+  {
+    const auto index = entities_set_.get(entity);
+    entities_set_.erase(entity);
+    data_[index] = data_.back();
+    data_.pop_back();
   }
 
   [[nodiscard]] auto contains(Entity entity) const noexcept -> bool
@@ -49,8 +58,14 @@ public:
     return entities_set_.entities();
   }
 
+  [[nodiscard]] auto data() const noexcept -> const ValueType*
+  {
+    return data_.data();
+  }
+
 private:
   SparseSet<Entity> entities_set_;
+  std::vector<ValueType> data_;
 };
 
 } // namespace beyond
@@ -79,17 +94,43 @@ TEST_CASE("SparseMap", "[beyond.core.ecs.sparse_set]")
 
     const Entity entity = 42;
     const float data = 3.14f;
+    const float data2 = 2.16f;
 
     WHEN("Insert that entity to the sparse map")
     {
       sm.insert(entity, data);
 
-      THEN("You can find this entity in the sparse set")
+      THEN("You can find this entity and its data in the sparse map")
       {
         REQUIRE(sm.contains(entity));
         const auto index = sm.get(entity);
         REQUIRE(index == 0);
         REQUIRE(sm.entities()[index] == entity);
+        REQUIRE(sm.data()[index] == Approx(data));
+      }
+
+      AND_WHEN("Delete that entity in the sparse map")
+      {
+        sm.erase(entity);
+        THEN("The sparse map will no longer contain that entity")
+        {
+          REQUIRE(sm.size() == 0);
+          REQUIRE(!sm.contains(entity));
+        }
+
+        AND_WHEN(
+            "Reconstructs that entity in the sparse map with different data")
+        {
+          sm.insert(entity, data2);
+
+          THEN("You can find this entity in the sparse map")
+          {
+            REQUIRE(sm.contains(entity));
+            const auto index = sm.get(entity);
+            REQUIRE(sm.entities()[index] == entity);
+            REQUIRE(sm.data()[index] == Approx(data2));
+          }
+        }
       }
     }
   }
